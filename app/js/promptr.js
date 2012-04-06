61f0a5e00d5a4f4bdb7c825ccdead669b7c9fb17
Luke Lee <durdenmisc@gmail.com>
2012-04-06 11:55:42 -0500

    Make options checkboxes, pretty it up

--- app/js/promptr.js
+++ app/js/promptr.js
@@ -1,41 +1,62 @@
 $(document).ready(function() {
     var options = new Array();
 
-    options['\\a'] = 'The ASCII bell character (you can also type \007)';
-    options['\\d'] = 'Date in "Wed Sep 06" format';
-    options['\\e'] = 'ASCII escape character (you can also type \033)';
-    options['\\h'] = 'First part of hostname (such as "mybox")';
-    options['\\H'] = 'Full hostname (such as "mybox.mydomain.com")';
-    options['\\j'] = 'The number of processes you\'ve suspended in this ' +
-                    'shell by hitting ^Z';
-    options['\\l'] = 'The name of the shell\'s terminal device (such as ' +
-                    '"ttyp4")';
-    options['\\n'] = 'Newline';
-    options['\\r'] = 'Carriage return';
-    options['\\s'] = 'The name of the shell executable (such as "bash")';
-    options['\\t'] = 'Time in 24-hour format (such as "23:01:01")';
-    options['\\T'] = 'Time in 12-hour format (such as "11:01:01")';
-    options['\\@'] = 'Time in 12-hour format with am/pm';
-    options['\\u'] = 'Your username';
-    options['\\v'] = 'Version of bash (such as 2.04)';
-    options['\\V'] = 'Bash version, including patchlevel';
-    options['\\w'] = 'Current working directory (such as "/home/drobbins")';
-    options['\\W'] = 'The "basename" of the current working directory (such ' +
-                    'as drobbins")';
-    options['\\!'] = 'Current command\'s position in the history buffer';
-    options['\\#'] = 'Command number (this will count up at each prompt, as ' +
-                    'long as you type something)';
+    options['bell'] = {'code': '\\a',
+                       'help': 'The ASCII bell character'};
 
+    options['date'] = {'code': '\\d', 'help': 'Wed Sep 06 format'};
+    options['short_hostname'] = {'code': '\\h',
+                                 'help': 'First part of hostname ex: mybox'};
+    options['long_hostname'] = {'code': '\\H',
+                                'help': 'Full hostname ex: mybox.mydomain.com'};
+    options['processes'] = {'code': '\\j',
+                            'help': 'The number of processes you\'ve ' + 
+                                    'suspended in this shell by hitting ^Z'};
+    options['term'] = {'code': '\\l',
+                       'help': 'The name of the shell\'s terminal device ' +
+                               'ex: ttyp4'};
+    options['newline'] = {'code': '\\n', 'help': 'Newline'};
+    options['return'] = {'code': '\\r', 'help': 'Carriage return'};
 
-    html = '<h2>Load it up</h2>' +
-           '<select name="options" class="options">';
+    options['shell_name'] = {'code': '\\s',
+                             'help': 'The name of the shell executable ex: bash'};
+    options['24_hour_military'] = {'code': '\\t',
+                                   'help': 'Time in 24-hour format ex: 23:01:01'};
+    options['12_hour'] = {'code': '\\T',
+                          'help': 'Time in 12-hour format ex: 11:01:01'};
+    options['12_hour_std'] = {'code': '\\@',
+                              'help': 'Time in 12-hour format with am/pm'};
+    options['username'] = {'code': '\\u', 'help': 'Username'};
+    options['shell_version'] = {'code': '\\v',
+                                'help': 'Version of bash ex: 2.04'};
+    options['shell_patchlevel'] = {'code': '\\V',
+                                   'help': 'Bash version, including patchlevel'};
+    options['cwd'] = {'code': '\\w',
+                      'help': 'Current working directory ex: /home/durden'};
+    options['basename'] = {'code': '\\W',
+                           'help': 'The basename of the current working ' +
+                                   'directory ex: durden'};
+    options['history_pos'] = {'code': '\\!',
+                              'help': 'Current command\'s position in the ' +
+                                      'history buffer'};
+
+    options['cmd_num'] = {'code': '\\#',
+                          'help': 'Command number (this will count up at ' +
+                                  'each prompt, as long as you type something)'};
+
+    html = '<h2>Load it up</h2><div class="control-group">' +
+           '<div class="controls">';
 
     for (var option in options) {
-        html += '<option value="' + option + '">' + options[option] +
-                '</option>';
+        html += '<label class="control-label checkbox" title="' + options[option]['help'] +
+                '"> <input type="checkbox" name="' + option + 
+                        '" class="chkbox"/>' + option +
+                '</label>';
     }
-    html += '</select>';
+
+    html += '</div></div>';
 
     var first_child = $('#steps li:first-child');
     $('#steps li:first-child').append(html);
+    $('label[title]').tooltip();
 });
