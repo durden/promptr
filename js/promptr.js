6ae2747be23c502678d26166d16a3d24ae29cfe3
Luke Lee <durdenmisc@gmail.com>
2012-04-07 13:59:17 -0500

    Add clippy js to copy to clipboard, reogranize js code for upcoming colors

--- js/promptr.js
+++ js/promptr.js
@@ -1,87 +1,8 @@
 $(document).ready(function() {
-
-    function order_step() {
-        var html = '<ul id="sortable" class="nav nav-tabs nav-stacked">';
-        $("input:checked").each(function() {
-            var option = $(this).attr('name');
-            html += '<li class="ui-state-default"><a href="#" title="' +
-                    options[option]['help'] + '">' + option + '</a></li>';
-        });
-
-        html += '</ul>';
-        $('#steps li:first-child').replaceWith(html);
-
-        /* Drag/drop sorting */
-        $("#sortable").sortable({
-            revert: true
-        });
-        $("#draggable").draggable({
-            connectToSortable: "#sortable",
-            helper: "clone",
-            revert: "invalid"
-        });
-        $("ul, li").disableSelection();
-    };
-
-    function color_step() {
-        colors = new Array('ffffff', '963400', '986536', '646809', '036400',
-                        '34696d', '00009b', '303498', '000000');
-
-        var ii = 0;
-        //colors = '<p id="color">' +
-            //'<label>' +
-                //'Pick a colour<br />' +
-                //'<select name="colour">' +
-                    //'<option value="ffffff">#ffffff</option>' +
-                    //'<option value="963400">#963400</option>' +
-                    //'<option value="986536">#986536</option>' +
-                    //'<option value="646809">#646809</option>' +
-                    //'<option value="036400">#036400</option>' +
-                    //'<option value="34696d">#34696d</option>' +
-                    //'<option value="00009b">#00009b</option>' +
-                    //'<option value="303498">#303498</option>' +
-                    //'<option value="000000">#000000</option>' +
-                //'</select>' +
-            //'</label>' +
-        //'</p>';
-
-
-        $('#sortable').children().each(function() {
-            color_html = '<span id="color' + ii + '" class="pull-right">' +
-                            '<label>' +
-                                '<select name="colour">';
-
-            for (var color in colors) {
-                color_html += '<option value="' + colors[color] + '">#' +
-                                colors[color] + '</option>';
-            }
-
-            color_html += '</select></label></span>';
-            $(this).children('a').first().append(color_html);
-            ii++;
-        });
-
-        $('select[name="colour"]').colourPicker({
-            ico: 'img/jquery.colourPicker.gif',
-            title: false
-        });
-     }
-
-    $('#button').click(function() {
-        if ($(this).text() == 'Order \'em') {
-            order_step();
-            $(this).text('Color \'em');
-            $('a[title]').tooltip();
-        } else {
-            color_step();
-        }
-   });
-
     var options = new Array();
 
     options['bell'] = {'code': '\\a',
                        'help': 'The ASCII bell character'};
-
     options['date'] = {'code': '\\d', 'help': 'Wed Sep 06 format'};
     options['short_hostname'] = {'code': '\\h',
                                  'help': 'First part of hostname ex: mybox'};
@@ -122,19 +43,103 @@ $(document).ready(function() {
                           'help': 'Command number (this will count up at ' +
                                   'each prompt, as long as you type something)'};
 
+    /* START */
+
     html = '<h2>Load it up</h2><div class="control-group">' +
            '<div class="controls">';
 
     for (var option in options) {
-        html += '<label class="control-label checkbox" title="' + options[option]['help'] +
-                '"> <input type="checkbox" name="' + option + 
-                        '" class="chkbox"/>' + option +
-                '</label>';
+        html += '<label class="control-label checkbox" title="' +
+                options[option]['help'] + '"> <input type="checkbox" name="' +
+                option + '" class="chkbox"/>' + option + '</label>';
     }
 
     html += '</div></div>';
 
     var first_child = $('#steps li:first-child');
     $('#steps li:first-child').append(html);
+
     $('label[title]').tooltip();
+    $('span[title]').tooltip();
+
+    /* Only show the prompt when we've successfully built it up */
+    $('#prompt').hide();
+
+    $('#button').click(function() {
+        if ($(this).text() == 'Order \'em') {
+            order_step();
+            $(this).text('Color \'em');
+            $('a[title]').tooltip();
+        } else {
+            compile_prompt();
+            // Not fully implemented yet...
+            //color_step();
+        }
+    });
+
+    function order_step() {
+        var html = '<ul id="sortable" class="nav nav-tabs nav-stacked">';
+        $("input:checked").each(function() {
+            var option = $(this).attr('name');
+            html += '<li class="ui-state-default"><a href="#" title="' +
+                    options[option]['help'] + '" name="' + option + '">' +
+                    option + '</a></li>';
+        });
+
+        html += '</ul>';
+        $('#steps li:first-child').replaceWith(html);
+
+        /* Drag/drop sorting */
+        $("#sortable").sortable({
+            revert: true
+        });
+        $("#draggable").draggable({
+            connectToSortable: "#sortable",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("ul, li").disableSelection();
+    };
+
+    function compile_prompt() {
+        var text_prompt = '';
+        $('#sortable').children().each(function() {
+            var option = $(this).children().first().attr('name');
+            text_prompt += options[option]['code'];
+        });
+
+        $('#prompt-input').val(text_prompt);
+        $('#prompt').show();
+
+        var export_text = 'export PS1="' + text_prompt + '"';
+        $('.add-on').append('<span class="clippy">' + export_text + '</span>');
+        $('.clippy').clippy();
+    }
+
+    function color_step() {
+        colors = new Array('ffffff', '963400', '986536', '646809', '036400',
+                        '34696d', '00009b', '303498', '000000');
+        var ii = 0;
+
+        $('#sortable').children().each(function() {
+            color_html = '<span id="color' + ii + '" class="pull-right">' +
+                            '<label>' +
+                                '<select name="colour">';
+
+            for (var color in colors) {
+                color_html += '<option value="' + colors[color] + '">#' +
+                                colors[color] + '</option>';
+            }
+
+            color_html += '</select></label></span>';
+            $(this).children('a').first().append(color_html);
+            ii++;
+        });
+
+        $('select[name="colour"]').colourPicker({
+            ico: 'img/jquery.colourPicker.gif',
+            title: false
+        });
+     }
+
 });
