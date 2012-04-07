7081013bda5eed1780ad1f28b2c0a63289516116
Luke Lee <durdenmisc@gmail.com>
2012-04-07 09:43:52 -0500

    Add color picker

---
+++ js/jquery.colourPicker.js
@@ -0,0 +1,120 @@
+/***
+@title:
+Colour Picker
+
+@version:
+2.0
+
+@author:
+Andreas Lagerkvist
+
+@date:
+2008-09-16
+
+@url:
+http://andreaslagerkvist.com/jquery/colour-picker/
+
+@license:
+http://creativecommons.org/licenses/by/3.0/
+
+@copyright:
+2008 Andreas Lagerkvist (andreaslagerkvist.com)
+
+@requires:
+jquery, colour_picker.css, jquery.colourPicker.gif
+***/
+
+jQuery.fn.colourPicker = function (conf) {
+	// Config for plug
+	var config = jQuery.extend({
+		id:			'jquery-colour-picker',	// id of colour-picker container
+		ico:		'ico.gif',				// SRC to colour-picker icon
+		title:		'Pick a colour',		// Default dialogue title
+		inputBG:	true,					// Whether to change input's bkg
+		speed:		500,					// Speed of dialogue-animation
+		openTxt:	'Open colour picker'
+	}, conf);
+
+	// Inverts a hex-colour
+	var hexInvert = function (hex) {
+		var r = hex.substr(0, 2);
+		var g = hex.substr(2, 2);
+		var b = hex.substr(4, 2);
+
+		return 0.212671 * r + 0.715160 * g + 0.072169 * b < 0.5 ? 'ffffff' : '000000'
+	};
+
+	// Add the colour-picker dialogue if not added
+	var colourPicker = jQuery('#' + config.id);
+
+	if (!colourPicker.length) {
+		colourPicker = jQuery('<div id="' + config.id +
+                              '"></div>').appendTo(document.body).hide();
+
+		// Remove the colour-picker if you click outside it (on body)
+		jQuery(document.body).click(function(event) {
+			if (!(jQuery(event.target).is('#' + config.id) || jQuery(event.target).parents('#' + config.id).length)) {
+				colourPicker.hide(config.speed);
+			}
+		});
+	}
+
+	// For every select passed to the plug-in
+	return this.each(function () {
+		// Insert icon
+		var select	= jQuery(this);
+		var icon	= jQuery('<a href="#"><img src="' + config.ico + 
+                        '" alt="' + config.openTxt +
+                        '" /></a>').insertAfter(select);
+		var loc		= '';
+
+		// Build a list of colours based on the colours in the select
+		jQuery('option', select).each(function () {
+			var option	= jQuery(this);
+			var hex		= option.val();
+			var title	= option.text();
+
+			loc += '<li><a href="#" title="' 
+					+ title 
+					+ '" rel="' 
+					+ hex 
+					+ '" style="background: #' 
+					+ hex 
+					+ '; colour: ' 
+					+ hexInvert(hex) 
+					+ ';">' 
+					+ title 
+					+ '</a></li>';
+		});
+
+		// Remove select
+		select.remove();
+
+		// When you click the icon
+		icon.click(function () {
+            // Show the colour-picker next to the icon and fill it with the
+            // colours in the select that used to be there
+			var iconPos	= icon.offset();
+			var heading	= config.title ? '<h2>' + config.title + '</h2>' : '';
+
+			colourPicker.html(heading + '<ul>' + loc + '</ul>').css({
+				position: 'absolute', 
+				left: iconPos.left + 'px', 
+				top: iconPos.top + 'px'
+			}).show(config.speed);
+
+			// When you click a colour in the colour-picker
+			jQuery('a', colourPicker).click(function () {
+				// The hex is stored in the link's rel-attribute
+				var hex = jQuery(this).attr('rel');
+
+				// Hide the colour-picker and return false
+				colourPicker.hide(config.speed);
+
+				return false;
+			});
+
+			return false;
+		});
+	});
+};
--- js/promptr.js
+++ js/promptr.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $('#button').click(function() {
+
+    function order_step() {
         var html = '<ul id="sortable" class="nav nav-tabs nav-stacked">';
         $("input:checked").each(function() {
             var option = $(this).attr('name');
@@ -11,17 +12,69 @@ $(document).ready(function() {
         $('#steps li:first-child').replaceWith(html);
 
         /* Drag/drop sorting */
-		$( "#sortable" ).sortable({
-			revert: true
-		});
-		$( "#draggable" ).draggable({
-			connectToSortable: "#sortable",
-			helper: "clone",
-			revert: "invalid"
-		});
-		$( "ul, li" ).disableSelection();
-        $(this).text('Color \'em');
-        $('a[title]').tooltip();
+        $("#sortable").sortable({
+            revert: true
+        });
+        $("#draggable").draggable({
+            connectToSortable: "#sortable",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("ul, li").disableSelection();
+    };
+
+    function color_step() {
+        colors = new Array('ffffff', '963400', '986536', '646809', '036400',
+                        '34696d', '00009b', '303498', '000000');
+
+        var ii = 0;
+        //colors = '<p id="color">' +
+            //'<label>' +
+                //'Pick a colour<br />' +
+                //'<select name="colour">' +
+                    //'<option value="ffffff">#ffffff</option>' +
+                    //'<option value="963400">#963400</option>' +
+                    //'<option value="986536">#986536</option>' +
+                    //'<option value="646809">#646809</option>' +
+                    //'<option value="036400">#036400</option>' +
+                    //'<option value="34696d">#34696d</option>' +
+                    //'<option value="00009b">#00009b</option>' +
+                    //'<option value="303498">#303498</option>' +
+                    //'<option value="000000">#000000</option>' +
+                //'</select>' +
+            //'</label>' +
+        //'</p>';
+
+
+        $('#sortable').children().each(function() {
+            color_html = '<span id="color' + ii + '" class="pull-right">' +
+                            '<label>' +
+                                '<select name="colour">';
+
+            for (var color in colors) {
+                color_html += '<option value="' + colors[color] + '">#' +
+                                colors[color] + '</option>';
+            }
+
+            color_html += '</select></label></span>';
+            $(this).children('a').first().append(color_html);
+            ii++;
+        });
+
+        $('select[name="colour"]').colourPicker({
+            ico: 'img/jquery.colourPicker.gif',
+            title: false
+        });
+     }
+
+    $('#button').click(function() {
+        if ($(this).text() == 'Order \'em') {
+            order_step();
+            $(this).text('Color \'em');
+            $('a[title]').tooltip();
+        } else {
+            color_step();
+        }
    });
 
     var options = new Array();
